bf134f190274f272cb544d82677fe42d65996c5a
VishalChougale1998 <chougalevishal1998@gmail.com>
2026-04-24 10:18:42 +0530

    vvvv

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,64 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
+
+// function Navbar() {
+
+//     return (
+//         <div
+//             style={{
+//                 width: "100%",
+//                 background: "linear-gradient(90deg, #0f2027, #203a43, #2c5364)",
+//                 // background: "linear-gradient(90deg, pink, #22cd44, #4da8cf)",
+//                 color: "white",
+//                 display: "flex",
+//                 justifyContent: "space-between",
+//                 alignItems: "center",
+//                 padding: "15px 40px",
+//                 boxSizing: "border-box",
+//                 position: "fixed",
+//                 top: 0,
+//                 left: 0,
+//                 zIndex: 1000
+//             }}
+//         >
+
+//             <img src="/logo.jpeg" alt="logo" style={{ height: "80px" }} />
+
+//             <div style={{ display: "flex", gap: "20px" }}>
+//                 <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+//                     Home
+//                 </Link>
+
+//                 <Link to="/admin" style={{ color: "white", textDecoration: "none" }}>
+//                     Admin Login
+//                 </Link>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Navbar;
+
+
+// =======================================
+
+
+
+
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.jpeg"; // ✅ correct import
 
 function Navbar() {
-
     return (
         <div
             style={{
                 width: "100%",
                 background: "linear-gradient(90deg, #0f2027, #203a43, #2c5364)",
-                // background: "linear-gradient(90deg, pink, #22cd44, #4da8cf)",
                 color: "white",
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "center",
-                padding: "15px 40px",
+                padding: "15px 20px",
                 boxSizing: "border-box",
                 position: "fixed",
                 top: 0,
@@ -22,14 +67,43 @@ function Navbar() {
             }}
         >
 
-            <img src="/logo.jpeg" alt="logo" style={{ height: "80px" }} />
+            {/* ✅ LOGO */}
+            <img
+                src={logo}
+                alt="logo"
+                style={{
+                    height: "60px",
+                    objectFit: "contain"
+                }}
+            />
 
-            <div style={{ display: "flex", gap: "20px" }}>
-                <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+            {/* ✅ NAV LINKS */}
+            <div
+                style={{
+                    display: "flex",
+                    gap: "20px",
+                    alignItems: "center"
+                }}
+            >
+                <Link
+                    to="/"
+                    style={{
+                        color: "white",
+                        textDecoration: "none",
+                        fontWeight: "500"
+                    }}
+                >
                     Home
                 </Link>
 
-                <Link to="/admin" style={{ color: "white", textDecoration: "none" }}>
+                <Link
+                    to="/admin"
+                    style={{
+                        color: "white",
+                        textDecoration: "none",
+                        fontWeight: "500"
+                    }}
+                >
                     Admin Login
                 </Link>
             </div>
