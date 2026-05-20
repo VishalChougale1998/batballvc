ba509986c536f26585ed2f8810fa90b373d43ae7
VishalChougale1998 <chougalevishal1998@gmail.com>
2026-05-20 17:27:43 +0530

    yt addition 4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -63,15 +63,15 @@ function Home() {
 
                 </div>
                 {/* ...................... */}
-                <div class="video-container">
+                <div className="video-container">
                     <iframe
                         src="https://www.youtube.com/embed/bmklhx9bgX4?si=PWDKwg4yV-OTIvqE"
                         title="YouTube video player"
-                        frameborder="0"
+                        frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin"
-                        allowfullscreen>
-                    </iframe>
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                    />
                 </div>
 
                 {/* ............................ */}
